d8eae5a73546696d8c62ceac00436e3277fe4fab
2109106115_Rona Nurhijja Qonita <113363627+2109106115ronanurhijjaqonita@users.noreply.github.com>
2022-11-02 09:35:52 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const warna= document.getElementById("warna");
+warna.style.color= '#00FFD1';
+// DOM dan addEventListener
+const putar_film = document.getElementById('cari');
+putar_film.addEventListener("click",
+    function(){
+        alert("Data Base Musik Masih Kosong!" );
+    },
+)
